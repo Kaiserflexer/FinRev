4455e5b16ca0392d04056e43ff3696ac6079a51f
Alex <desert1thunder@gmail.com>
2025-08-15 15:22:00 +0300

    test: clean up App tests and mocks

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,46 +1,17 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-window.matchMedia = window.matchMedia || function (query) {
-  return {
-    matches: false,
-    media: query,
-    onchange: null,
-
-// Polyfill window.matchMedia for Ant Design
+// Polyfill window.matchMedia for Ant Design components
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
   value: (query) => ({
     matches: false,
     media: query,
     onchange: null,
-    addListener: () => {},
-    removeListener: () => {},
-    addEventListener: () => {},
-    removeEventListener: () => {},
-    dispatchEvent: () => false,
-  }),
-});
-
-// Ant Design relies on matchMedia. Provide a basic mock for the test environment.
-if (typeof window !== 'undefined' && !window.matchMedia) {
-  window.matchMedia = () => ({
-    matches: false,
-    media: '',
-    onchange: null,
-
     addListener: jest.fn(),
     removeListener: jest.fn(),
     addEventListener: jest.fn(),
     removeEventListener: jest.fn(),
     dispatchEvent: jest.fn(),
-
-  };
-};
-
-  });
-}
+  }),
+});
 
